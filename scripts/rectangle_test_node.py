11603258f2536f9817a86e6c223fcad96da36c19
Hoyoung-Jung-17 <wjdghdud17@gmail.com>
2025-08-07 16:39:02 +0900

    update

--- scripts/rectangle_test_node.py
+++ scripts/rectangle_test_node.py
@@ -11,7 +11,7 @@ class RectTest(Node):
     def __init__(self):
         super().__init__('rect_test_node')
 
-        self.declare_parameter('corners', [0.0, 0.0, 3.0, 0.0, 3.0, 3.0, 0.0, 3.0])
+        self.declare_parameter('corners', [3.0, 0.0, 3.0, 3.0, 0.0, 3.0, 0.0, 0.0])
         self.declare_parameter('goal_tolerance', 0.3)
         
         points = self.get_parameter('corners').value
@@ -19,7 +19,7 @@ class RectTest(Node):
         self.corners = [(points[i], points[i+1]) for i in range(0, len(points), 2)]
         self.tolerance = self.get_parameter('goal_tolerance').value
         self.current_index = 0
-        self.currnet_pose = None
+        self.current_pose = None
         self.lap_count = 0
 
         # Subscriber 
@@ -35,11 +35,11 @@ class RectTest(Node):
         self.publish_goal()
 
     def odom_cb(self, msg: Odometry):
-        self.currnet_pose = msg.pose.pose.position
+        self.current_pose = msg.pose.pose.position
 
     def publish_goal(self):
         x, y = self.corners[self.current_index]
-        goal = PointStamped
+        goal = PointStamped()
         goal.header.frame_id = 'map'
         goal.header.stamp = self.get_clock().now().to_msg()
         goal.point.x = x
@@ -48,23 +48,25 @@ class RectTest(Node):
         self.goal_pub.publish(goal)
 
     def timer_cb(self):
+        if self.current_pose is None:
+            return 
         gx, gy = self.corners[self.current_index]     
         dx = gx - self.current_pose.x
         dy = gy - self.current_pose.y
         dist = math.hypot(dx, dy)
 
-        if dist <= self.tolerance:
-            self.currnet_index = (self.current_index +1) % len(self.corners)
-            self.publish_goal()
         # if dist <= self.tolerance:
-        #     next_index = (self.current_index +1) % len(self.corners)
-        #     if lap_count == 0:
-        #         lap_count +=1
-        #         if lap_count >=1:
-        #             self.timer.cancle()
-        #             return
-        #     self.current_index = next_index
+        #     self.current_index = (self.current_index +1) % len(self.corners)
         #     self.publish_goal()
+        if dist <= self.tolerance:
+            next_index = (self.current_index +1) % len(self.corners)
+            if lap_count == 0:
+                lap_count +=1
+                if lap_count >=2:
+                    self.timer.cancle()
+                    return
+            self.current_index = next_index
+            self.publish_goal()
 
 
 
